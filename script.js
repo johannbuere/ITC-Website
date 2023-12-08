44c3cb95cf67100297a83b216d795dded8cd6b50
Johann Buere <johannbuere@gmail.com>
2023-12-08 14:39:48 +0800

    added back to top btn and portfolio btn

--- script.js
+++ script.js
@@ -41,3 +41,44 @@ inputs.forEach(ipt => {
     });
 
 });
+
+//Javascript to smoothen page scroll
+document.addEventListener('DOMContentLoaded', function() {
+    var portfolioButtons = document.querySelectorAll('.portfolio-btn');
+
+    portfolioButtons.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            var targetSectionId = button.querySelector('a').getAttribute('href').substring(1);
+
+            var targetSection = document.getElementById(targetSectionId);
+
+            if (targetSection) {
+                targetSection.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
+
+/* scroll back 2 top */
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+      scrollProgress.style.display = "grid";
+    } else {
+      scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+      document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#666666 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+  };
+  window.onscroll = calcScrollValue;
+  window.onload = calcScrollValue;
